69cd29ad06a3e9e81df752c0ad8cb4324f98ce37
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-02-27 16:53:41 +0300

    Возведение в степень с помощью функции

--- Example005_Number_square/Program.cs
+++ Example005_Number_square/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine("Введите первое число: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
-if(num1*num1==num2 || num2*num2 == num1)
+if(Math.Pow(num1,2) == num2 || Math.Pow(num2,2)==num1)
 {
 Console.WriteLine("Да");
 }
